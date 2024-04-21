63f62b8cffcad768e416c6b6a0634ae5992d3168
Adityawaghmare80 <148642692+Adityawaghmare80@users.noreply.github.com>
2024-04-21 02:39:12 -0700

    td-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'templaTedrivenForm';
+  @ViewChild('signUp') signUp!: NgForm;
+
+  mocArr = [
+    {contactMode: 'Email', id: 123},
+    {contactMode: 'Phone No', id: 124}
+  ]
+
+  userDetails = {
+      "username": "adityawaghmare",
+      "email": "aditya@gmail.com",
+      "passwordRecoveryQuestion": "favouriteCountry",
+      "answer": "Spain",
+      "isSubscribed": true,
+      "modeOfContact": "Phone No"
+  }
+
+  constructor(){
+
+  }
+  ngOnInit(): void {
+    
+  }
+
+  onSignUp(signUp: NgForm){
+    // console.log(signUp);
+    console.log(signUp.value);
+  }
+
+  onEdit(){
+    this.signUp.form.patchValue(this.userDetails)
+  }
+
+ 
+  
 }
